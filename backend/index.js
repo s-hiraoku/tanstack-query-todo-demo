2510598c091ba38d99a8b0b443b222add9e62907
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-08-12 10:43:35 +0900

    Add cors middleware to the express app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const port = 3001;
+
+app.use(cors());
 
 const prisma = new PrismaClient();
 app.get("/todos", async (req, res) => {
